fc9b7f86f5c66c866bd183b964913ae7530c7da2
Sean W <104464288+swilso59@users.noreply.github.com>
2023-09-06 18:00:45 -0600

    Update code.js
    Fixed the sum variable.

--- code.js
+++ code.js
@@ -1,7 +1,11 @@
 function sum(a) {
-    var sum = a[0];
+    var sum = 0;
     for(var i = 0; i < a.length; i++) {
         sum += a[i];
     }
     return sum;
+
 }
+
+//result = sum([1,2,3]);
+//console.log(result)
